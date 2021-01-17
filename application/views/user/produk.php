936e6d1304f9463b3eb2003330581efdaac4c9f6
rifkimulyafajar <rifkifajar25@gmail.com>
2021-01-17 14:59:58 +0700

    update produk dan jasa.php

--- application/views/user/produk.php
+++ application/views/user/produk.php
@@ -15,52 +15,163 @@
 	                </div>
 	            </div>
 
-
-			    <div class="whole-wrap">
-		            <div class="container box_1170">
-		                <div class="section-top-border">
-							<div class="row gallery-item">
-								<div class="col-md-3 text-center">
-									<a href="<?php echo base_url('asset/user/img/icon/ikan-kakap-merah.png') ?>" class="img-pop-up">
-										<div class="single-gallery-image" style="background: url(<?php echo base_url('asset/user/img/icon/ikan-kakap-merah.png') ?>);">
-										</div>
-									</a>
-									<h5>Ikan Kakap Merah</h5>
-									<p><i>Red Snapper</i></p>
-									<p><i>Lutjanius Campechanus</i></p>
-								</div>
-								<div class="col-md-3 text-center">
-									<a href="<?php echo base_url('asset/user/img/icon/ikan-tuna.png') ?>" class="img-pop-up">
-										<div class="single-gallery-image" style="background: url(<?php echo base_url('asset/user/img/icon/ikan-tuna.png') ?>);">
-										</div>
-									</a>
-									<h5>Ikan Tuna</h5>
-									<p><i>Bluefin Tuna</i></p>
-									<p><i>Thunnus Thynnus</i></p>
-								</div>
-								<div class="col-md-3 text-center">
-									<a href="<?php echo base_url('asset/user/img/icon/ikan-kerapu.png') ?>" class="img-pop-up">
-										<div class="single-gallery-image" style="background: url(<?php echo base_url('aasset/user/img/icon/ikan-kerapu.png') ?>);">
-										</div>
-									</a>
-									<h5>Ikan Kerapu</h5>
-									<p><i>Grouper Fish</i></p>
-									<p><i>Epinephelinae</i></p>
-								</div>
-								<div class="col-md-3 text-center">
-									<a href="<?php echo base_url('asset/user/img/icon/ikan-layur.png') ?>" class="img-pop-up">
-										<div class="single-gallery-image" style="background: url(<?php echo base_url('asset/user/img/icon/ikan-layur.png') ?>);">
-										</div>
-									</a>
-									<h5>Ikan Layur</h5>
-									<p><i>Ribbon Fish </i></p>
-									<p><i>Trichiurus Lepturus</i></p>
-								</div>
-							</div>
+	            <br><br>
+	            <div class="row">
+	            	<div class="col-xl-12 section_title text-center">
+	            		<h1>Produk Kami</h1> <br>
+	            	</div>
+	            	<div class="col-xl-3 section_title text-center">
+	            		<div class="card">
+						  <img src="<?php echo base_url('asset/user/img/icon/produk/ikan-kakap-merah.png') ?>" class="card-img-top">
+						  <div class="card-body">
+						    <h5 class="card-title">Ikan Kakap Merah</h5>
+						    <p class="card-text">
+						    	<i>Red Snapper <br>
+								Lutjanius Campechanus</i>
+							</p>
+						  </div>
 						</div>
-					</div>
-				</div>
+	            	</div>
+	            	<div class="col-xl-3 section_title text-center">
+	            		<div class="card">
+						  <img src="<?php echo base_url('asset/user/img/icon/produk/ikan-tuna.png') ?>" class="card-img-top">
+						  <div class="card-body">
+						    <h5 class="card-title">Ikan Tuna</h5>
+						    <p class="card-text">
+						    	<i>Bluefin Tuna <br>
+								Thunnus Thynnus</i>
+							</p>
+						  </div>
+						</div>
+	            	</div>
+	            	<div class="col-xl-3 section_title text-center">
+	            		<div class="card">
+						  <img src="<?php echo base_url('asset/user/img/icon/produk/ikan-kerapu.png') ?>" class="card-img-top">
+						  <div class="card-body">
+						    <h5 class="card-title">Ikan Kerapu</h5>
+						    <p class="card-text">
+						    	<i>Grouper Fish <br>
+								Epinephelinae</i>
+							</p>
+						  </div>
+						</div>
+	            	</div>
+	            	<div class="col-xl-3 section_title text-center">
+	            		<div class="card">
+						  <img src="<?php echo base_url('asset/user/img/icon/produk/ikan-layur.png') ?>" class="card-img-top">
+						  <div class="card-body">
+						    <h5 class="card-title">Ikan Layur</h5>
+						    <p class="card-text">
+						    	<i>Ribbon Fish<br>
+								Trichiurus Lepturus</i>
+							</p>
+						  </div>
+						</div>
+	            	</div>
+	            </div>
 
+	            <br>
+	            <div class="row">
+	            	<div class="col-xl-3 section_title text-center">
+	            		<div class="card">
+						  <img src="<?php echo base_url('asset/user/img/icon/produk/ikan-tenggiri.png') ?>" class="card-img-top">
+						  <div class="card-body">
+						    <h5 class="card-title">Ikan Tenggiri</h5>
+						    <p class="card-text">
+						    	<i>Mackerel<br>
+								Scomberomorini</i>
+							</p>
+						  </div>
+						</div>
+	            	</div>
+	            	<div class="col-xl-3 section_title text-center">
+	            		<div class="card">
+						  <img src="<?php echo base_url('asset/user/img/icon/produk/ikan-albakora.png') ?>" class="card-img-top">
+						  <div class="card-body">
+						    <h5 class="card-title">Ikan Albakora</h5>
+						    <p class="card-text">
+						    	<i>False Albacore<br>
+								Euthynnus Alletteratus</i>
+							</p>
+						  </div>
+						</div>
+	            	</div>
+	            	<div class="col-xl-3 section_title text-center">
+	            		<div class="card">
+						  <img src="<?php echo base_url('asset/user/img/icon/produk/cumi.png') ?>" class="card-img-top">
+						  <div class="card-body">
+						    <h5 class="card-title">Cumi - cumi</h5>
+						    <p class="card-text">
+						    	<i>Squid<br>
+								Decapodiformes</i>
+							</p>
+						  </div>
+						</div>
+	            	</div>
+	            	<div class="col-xl-3 section_title text-center">
+	            		<div class="card">
+						  <img src="<?php echo base_url('asset/user/img/icon/produk/sotong.png') ?>" class="card-img-top">
+						  <div class="card-body">
+						    <h5 class="card-title">Sotong</h5>
+						    <p class="card-text">
+						    	<i>Cuttlefish<br>
+								Sepiida</i>
+							</p>
+						  </div>
+						</div>
+	            	</div>
+	            </div>
 
+	            <br>
+	            <div class="row">
+	            	<div class="col-xl-3 section_title text-center">
+	            		<div class="card">
+						  <img src="<?php echo base_url('asset/user/img/icon/produk/ikan-lemadang.png') ?>" class="card-img-top">
+						  <div class="card-body">
+						    <h5 class="card-title">Ikan Lemadang</h5>
+						    <p class="card-text">
+						    	<i>Mahi-Mahi Fish<br>
+								Coryphaena Hippurus</i>
+							</p>
+						  </div>
+						</div>
+	            	</div>
+	            	<div class="col-xl-3 section_title text-center">
+	            		<div class="card">
+						  <img src="<?php echo base_url('asset/user/img/icon/produk/ikan-cakalang.png') ?>" class="card-img-top">
+						  <div class="card-body">
+						    <h5 class="card-title">Ikan Cakalang</h5>
+						    <p class="card-text">
+						    	<i>Skipjack Tuna<br>
+								Katsuwonus Pelamis</i>
+							</p>
+						  </div>
+						</div>
+	            	</div>
+	            	<div class="col-xl-3 section_title text-center">
+	            		<div class="card">
+						  <img src="<?php echo base_url('asset/user/img/icon/produk/udang.png') ?>" class="card-img-top">
+						  <div class="card-body">
+						    <h5 class="card-title">Udang</h5>
+						    <p class="card-text">
+						    	<i>Shrimp<br>
+								Penaeus Monodon</i>
+							</p>
+						  </div>
+						</div>
+	            	</div>
+	            	<div class="col-xl-3 section_title text-center">
+	            		<div class="card">
+						  <img src="<?php echo base_url('asset/user/img/icon/produk/gurita.png') ?>" class="card-img-top">
+						  <div class="card-body">
+						    <h5 class="card-title">Gurita Kemasan</h5>
+						    <p class="card-text">
+						    	<i>Octopus Ball Packed<br>
+								Octopus Vulgaris</i>
+							</p>
+						  </div>
+						</div>
+	            	</div>
+	            </div>
 			</div>
 	    </div>
